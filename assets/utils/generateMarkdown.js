8253650681d95a2819408e29809a634249803095
enigmawoman <112570078+enigmawoman@users.noreply.github.com>
2022-11-22 20:04:07 +0000

    small changes to main index.js file

--- assets/utils/generateMarkdown.js
+++ assets/utils/generateMarkdown.js
@@ -2,14 +2,19 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license !== "None") {
-   return `![badge](https://img.shields.io/badge/license${license}-orange)</br>`;
+   return `![badge](https://img.shields.io/badge/license-${license}-orange)</br>`;
   }
   return '';
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== "None" ) {
+    return '\n * [License](#license) \n'
+  }
+  return '';
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -31,27 +36,40 @@ function generateMarkdown(answers) {
   ${renderLicenseBadge(answers.license)}
 
  ## Description
+
  ${answers.description}
 
  ## Table of Contents
+ 
  * [Description](#description)
+ * [Installation](#installation)
+ * [Usage](#usage)
+ ${renderLicenseLink(answers.license)}
+ * [Contributing](#contributing)
+ * [Testing](#testing)
+ * [Questions](#questions)
 
  ## Installation
+
  ${answers.installation}
 
  ## Usage
+
  ${answers.usage}
 
  ${renderLicenseSection(answers.license)}
 
  ## Contributing
- The following have contributed to this project:
+
+ The following people have contributed to this project:
  ${answers.contributing}
 
  ## Testing
+
 ${answers.tests}
 
 ## Questions
+
 If you have any questions, reach out at [${answers.username}(https://github.com/${answers.username})</br>
 Alternativlely, send your questions to ${answers.email}
 `;
