e8858f1eb781f47490cfcc10c4bb38bf36192f6f
enigmawoman <112570078+enigmawoman@users.noreply.github.com>
2022-11-24 13:56:40 +0000

    code comments and readme file updated

--- assets/generateMarkdown.js
+++ assets/generateMarkdown.js
@@ -1,5 +1,5 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+// A function that returns a license badge based on which license is passed in fron the users answers
+// If there is no license, returns an empty string
 function renderLicenseBadge(license) {
   if (license !== "None") {
    return `![badge](https://img.shields.io/badge/license-${license}-orange)</br>`;
@@ -7,8 +7,8 @@ function renderLicenseBadge(license) {
   return '';
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
+// A function that returns the license link for the table of contents
+// If there is no license, returns an empty string
 function renderLicenseLink(license) {
   if (license !== "None" ) {
     return '\n * [License](#license) \n'
@@ -16,8 +16,8 @@ function renderLicenseLink(license) {
   return '';
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
+// A function that returns the license section of README using the answers provided by the user
+// If there is no license, returns a message to say the project is not covered under a license
 function renderLicenseSection(license) {
   if (license !== "None") {
     return `
@@ -27,7 +27,7 @@ function renderLicenseSection(license) {
   return 'This project is not covered under a license';
 }
 
-// TODO: Create a function to generate markdown for README
+// This function generates markdown for README and inputs the data taken from the questions the user answers to populate the file
 function generateMarkdown(answers) {
   return `
   # ${answers.title}
